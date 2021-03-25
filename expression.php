a253486aa8f365941f813e0504f0b7a9b2265735
namanh29 <namanh29tb@gmail.com>
2021-03-25 13:19:53 +0700

    a

---
+++ expression.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Expression example</title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        $gr1 = 50;
+        $gr2 = 100;
+        $gr3 = 150;
+        $aver = ($gr1+$gr2+$gr3)/3;
+        print "The average is $aver";
+        ?>
+    </body>
+</html>
