1a45f07a3a3e31f3d2b35d6873fc330552493664
GiangTran2k <71329920+GiangTran2k@users.noreply.github.com>
2021-03-28 22:09:46 +0700

    Add files via upload

---
+++ welcome.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["school"]; ?><br>
+Your email address is: <?php echo $_POST["class"]; ?><br>
+Your email address is: <?php echo $_POST["hobby"]; ?><br>
+
+</body>
+</html>
